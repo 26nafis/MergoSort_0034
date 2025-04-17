012bdbced06a3defb3b33b76b852a0b5d77bad9f
26nafis <spalanzaninafis@gmail.com>
2025-04-17 10:41:59 +0700

    membuat array dengan panjang 20 dan input array

--- mergosort.cpp
+++ mergosort.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 using namespace std;
+
+int arr[20], B[20];
+
+int n;
+
+void input() {
+    while (true) {
+        cout << "masukan panjang element array: ";
+        cin >> n;
+
+        if (n<= 20){
+            break;
+        }
+        else{
+            cout <<"/nmaksimal panjang array adalah 20";
+        }
+    }
+
+    cout <<"\n=============================";
+    cout << "\ninputkan isi element array";
+    cout << "\n============================="; << endl;
+
+    for (int i = 0; i < n; i++){
+        cout << "array index ke - " << i << "i ";
+        cin >> arr[i];
+    }
+}
+
+void mergesort(int low, int high) {
+
+    if (low >= high) { //step 1
+
+    }
+}
